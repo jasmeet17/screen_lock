baa30e445e302e0fdbc8f07b77a857e5feab0a04
Abhi <abhi@Abhis-MacBook-Pro.local>
2015-04-02 00:03:47 +0530

    Initial Commit

---
+++ Cards Lock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cards Lock
+//
+//  Created by Abhi on 02/04/15.
+//  Copyright (c) 2015 Abhi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
